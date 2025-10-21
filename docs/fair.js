b37fd9fa48397835f24a9f30b4fd1642d540bcd4
fairdao-coder <fairdao@hotmail.com>
2025-10-22 04:27:32 +0800

    save

--- docs/fair.js
+++ docs/fair.js
@@ -100,7 +100,7 @@ function processBindFor(element,json) {
             var reg = new RegExp("\{\-[^\}]+?\-\}", "g");
             let itemHtml = html.replace(reg, function (s) {
                 var old = s;
-                s = s.replace(new RegExp(key + "\."), "").replace(/\-\}/g, "").replace(/\{\-/g, "");
+                s = s.replace(new RegExp("item\."), "").replace(/\-\}/g, "").replace(/\{\-/g, "");
                 let val = eval("item." + s);
                 return val;
             });
